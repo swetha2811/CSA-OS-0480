aec610927feaf44b9cb1119dee62ecac338bfce6
swetha2811 <124872520+swetha2811@users.noreply.github.com>
2023-11-29 12:38:17 +0530

    Update EXP-9.C

--- EXP-9.C
+++ EXP-9.C
@@ -1,41 +1,20 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
-#include<semaphore.h>
-#include<unistd.h>
-sem_t room;
-sem_t chopstick[5];
-void * philosopher(void *);
-void eat(int);
-int main()
-{
-	int i,a[5];
-	pthread_t tid[5];
-	sem_init(&room,0,4);
-	for(i=0;i<5;i++)
-		sem_init(&chopstick[i],0,1);	
-	for(i=0;i<5;i++){
-		a[i]=i;
-		pthread_create(&tid[i],NULL,philosopher,(void *)&a[i]);
-	}
-	for(i=0;i<5;i++)
-		pthread_join(tid[i],NULL);
-}
-void * philosopher(void * num)
-{
-	int phil=*(int *)num;
-	sem_wait(&room);
-	printf("\nPhilosopher %d has entered room",phil);
-	sem_wait(&chopstick[phil]);
-	sem_wait(&chopstick[(phil+1)%5]);
-	eat(phil);
-	sleep(2);
-	printf("\nPhilosopher %d has finished eating",phil);
-	sem_post(&chopstick[(phil+1)%5]);
-	sem_post(&chopstick[phil]);
-	sem_post(&room);
-}
-void eat(int phil)
-{
-	printf("\nPhilosopher %d is eating",phil);
-}
+#include<stdio.h>  
+#include<stdlib.h>  
+#include<unistd.h>  
+#include<sys/shm.h>  
+#include<string.h>  
+int main()  
+{  
+int i;  
+void *shared_memory;  
+char buff[100];  
+int shmid;  
+shmid=shmget((key_t)2345, 1024, 0666|IPC_CREAT);   
+printf("Key of shared memory is %d\n",shmid);  
+shared_memory=shmat(shmid,NULL,0);    
+printf("Process attached at %p\n",shared_memory); 
+printf("Enter some data to write to shared memory\n");  
+read(0,buff,100); 
+strcpy(shared_memory,buff); 
+printf("You wrote : %s\n",(char *)shared_memory);  
+}  
